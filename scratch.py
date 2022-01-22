b5b89f9c449bc58c0e30a997ccce12b973141a28
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2022-01-22 13:58:34 +0200

    Randomly changing weights and biases for vertical data generated function

--- scratch.py
+++ scratch.py
@@ -1,15 +1,9 @@
-import math
-import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import vertical_data
 
-softmax_outputs = np.array([[0.7, 0.1, 0.2],
-                            [0.1, 0.5, 0.4],
-                            [0.02, 0.9, 0.08]])
+nnfs.init()
 
-class_targets = [0, 1, 1]
-
-neg_log = - np.log(softmax_outputs[
-                       range(len(softmax_outputs)), class_targets
-                   ])
-
-average_loss = np.mean(neg_log)
-print(average_loss)
+X, y = vertical_data(samples=100, classes=3)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
