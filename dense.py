e902db2adb0c4df28a68ff425c4336fb424b8241
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2022-01-27 18:54:40 +0200

    backward tracking with derivatives

--- dense.py
+++ dense.py
@@ -7,6 +7,10 @@ class Layer_Dense:
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
+        self.dinputs = None
+        self.dbiases = None
+        self.dweights = None
+        self.inputs = None
         self.output = None
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -15,3 +19,11 @@ class Layer_Dense:
     def forward(self, inputs):
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
--- relu.py
+++ relu.py
@@ -5,8 +5,18 @@ import numpy as np
 class Activation_ReLU:
     # Forward pass
     def __init__(self):
+        self.inputs = None
+        self.dinputs = None
         self.output = None
 
     def forward(self, inputs):
+        self.inputs = inputs
         # Calculate output values from inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
